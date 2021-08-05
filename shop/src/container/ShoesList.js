22f31a1353d61f661cffd28b2d1e04546e93961e
Minsung Kim <dolikart@naver.com>
2021-08-06 04:36:44 +0900

    상품 개수가 넘어갈때 버튼 비활성화 && ButtonUI 컴포넌트화

--- shop/src/container/ShoesList.js
+++ shop/src/container/ShoesList.js
@@ -11,39 +11,79 @@ import ShoesItem from '../component/ShoesItem';
 import Navigator from '../component/Navbar';
 
 const ShoesList = (props) => {
-	let [shoes, setShoes] = useState(Data);
-	let [wshoes, setWShoes] = useState(Data2);
+	let [shoes, setShoes] = useState(Data); //남자 상품의 데이터
+	let [wshoes, setWShoes] = useState(Data2); //여자 상품의 데이터
+	let [shoesNum, setShoesNum] = useState(Object.keys(Data).length); //남자 상품의 개수
+	let [wshoesNum, setWShoesNum] = useState(Object.keys(Data2).length); //여자 상품의 개수
+	let [btndisable, setBtnDisable] = useState(false); //상품의 개수가 넘어가면 남자카테고리 더보기 버튼 비활성화
+	let [wbtndisable, setWBtnDisable] = useState(false); //상품의 개수가 넘어가면 여자 카테고리 더보기 버튼 비활성화
 
+	/************** 렌더링 관련 컴포넌트 **************/
+	//props.num이 0이면 남자 화면 렌더링
 	const Man = () => {
 		return (
 			<div className="row">
 				{shoes.map((item, i) => {
+					//컴포넌트 반복
 					return <ShoesItem shoes={item} num={i} key={i}></ShoesItem>;
 				})}
 			</div>
 		);
 	};
+	//props.num이 1이면 여자 화면 렌더링
 	const Woman = () => {
 		return (
 			<div className="row">
 				{wshoes.map((item, i) => {
+					//컴포넌트 반복
 					return <ShoesItem shoes={item} num={i} key={i}></ShoesItem>;
 				})}
 			</div>
 		);
 	};
+	//더보기 버튼 UI  남자, 여자 버튼의 각각의 State를 props로 받는다.
+	const ButtonUI = (i, whosebtn) => {
+		return (
+			<Button
+				disabled={whosebtn}
+				type="primary"
+				style={{ margin: '4rem' }}
+				onClick={() => {
+					fetchData(i);
+				}}>
+				더보기
+			</Button>
+		);
+	};
 
+	/************** 더보기 버튼 클릭시 axios 작동 **************/
+	//axios로 추가데이터 받아오기 num:0 남자 num:1 여자
 	const fetchData = (i) => {
-		axios
-			.get('https://minsoftk.github.io/jsontest/test' + i + '.json')
-			.then((result) => {
-				console.log(result.data);
-				let newObj = [...wshoes, ...result.data];
-				setWShoes(newObj);
-			})
-			.catch(() => {
-				console.log('실패');
-			});
+		i
+			? axios // i === 1일때 여자 카테고리 더보기 버튼 클릭시
+					.get('https://minsoftk.github.io/jsontest/test' + i + '.json')
+					.then((result) => {
+						let newObj = [...wshoes, ...result.data]; //데이터 합치기
+						setWShoesNum(Data.length + result.data.length); //원래 Data와 추가된 데이터의 길이
+						if (newObj.length >= wshoesNum) setWBtnDisable('true'); //합친 데이터의 길이가 더 크다면 여자 카테고리 버튼 비활성화
+						setWShoes(newObj);
+						console.log(btndisable);
+					})
+					.catch(() => {
+						console.log('실패');
+					})
+			: axios // i === 0일때 남자 카테고리 더보기 버튼 클릭시
+					.get('https://minsoftk.github.io/jsontest/test' + i + '.json')
+					.then((result) => {
+						let newObj = [...shoes, ...result.data]; //데이터 합치기
+						setShoesNum(Data.length + result.data.length); //원래 Data와 추가된 데이터의 길이
+						if (newObj.length >= shoesNum) setBtnDisable('true'); //합친 데이터의 길이가 더 크다면 남자 카테고리 버튼 비활성화
+						setShoes(newObj);
+						console.log(btndisable);
+					})
+					.catch(() => {
+						console.log('실패');
+					});
 	};
 	return (
 		<>
@@ -52,14 +92,19 @@ const ShoesList = (props) => {
 				<div className="row">
 					{props.num === 1 ? <Woman></Woman> : <Man></Man>}
 				</div>
-				<Button
-					type="primary"
-					style={{ margin: '4rem' }}
-					onClick={() => {
-						fetchData(props.num);
-					}}>
-					더보기
-				</Button>
+				{props.num === 1 ? (
+					<ButtonUI i={props.num} whosebtn={wbtndisable}></ButtonUI>
+				) : (
+					<Button
+						disabled={btndisable}
+						type="primary"
+						style={{ margin: '4rem' }}
+						onClick={() => {
+							fetchData(props.num);
+						}}>
+						더보기
+					</Button>
+				)}
 			</div>
 		</>
 	);
