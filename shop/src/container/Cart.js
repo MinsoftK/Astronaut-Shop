b1238e5a90282c2c01ea32039565e645660c9f8d
Minsung Kim <dolikart@naver.com>
2021-08-09 15:59:48 +0900

    다수의 reducer 활용

--- shop/src/container/Cart.js
+++ shop/src/container/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
 					{props.state.map((item, i) => {
 						return (
 							<tr key={i}>
-								<td>{i + 1}</td>
+								<td>{item.id}</td>
 								<td>{item.name}</td>
 								<td>{item.remain}</td>
 
@@ -29,19 +29,37 @@ const Cart = (props) => {
 										}}>
 										+
 									</button>
+									<button
+										onClick={() => {
+											props.dispatch({ type: '수량감소' });
+										}}>
+										-
+									</button>
 								</td>
 							</tr>
 						);
 					})}
 				</tbody>
 			</Table>
+			{props.alert ? (
+				<div className="my-alert2">
+					<p>지금 구매하면 할인</p>
+					<button
+						onClick={() => {
+							props.dispatch({ type: '알림닫기' });
+						}}>
+						닫기
+					</button>
+				</div>
+			) : null}
 		</>
 	);
 };
 const 함수명 = (state) => {
 	console.log('state', state);
 	return {
-		state: state[2],
+		state: state.remainReducer,
+		alert: state.alert,
 	};
 };
 export default connect(함수명)(Cart);
--- shop/src/index.js
+++ shop/src/index.js
@@ -5,28 +5,47 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import Data from './Data/ShoesData';
 import Data2 from './Data/ShoesData2';
 
+// let reduxData = [
+// 	{ Data },
+// 	{ Data2 },
+// 	[
+// 		{ id: 0, name: '멋진신발', remain: 2 },
+// 		{ id: 1, name: '나이키신발', remain: 3 },
+// 	],
+// ];
+
+let btn_state = true;
+
+const alert = (state = alert, action) => {
+	if (action.type === '알림닫기') {
+		return !alert;
+	}
+	return state;
+};
+
 let reduxData = [
-	{ Data },
-	{ Data2 },
-	[
-		{ id: 0, name: '멋진신발', remain: 2 },
-		{ id: 1, name: '나이키신발', remain: 3 },
-	],
+	{ id: 0, name: '멋진신발', remain: 2 },
+	{ id: 1, name: '나이키신발', remain: 3 },
 ];
-const reducer = (state = reduxData, action) => {
+const remainReducer = (state = reduxData, action) => {
 	if (action.type === '수량증가') {
 		let copy = [...state];
-		copy[2][0].remain++;
+		copy[0].remain++;
+		return copy;
+	} else if (action.type === '수량감소') {
+		let copy = [...state];
+		if (copy[0].remain < 1) copy[0].remain = 0;
+		else copy[0].remain--;
 		return copy;
 	} else {
 		return state;
 	}
 };
-let store = createStore(reducer);
+let store = createStore(combineReducers({ remainReducer, alert }));
 
 ReactDOM.render(
 	<React.StrictMode>
